f7466e0f9fb97df3cd41a1ce72fc3985323f623d
Markus Meresma <markus.meresma@gmail.com>
2025-01-29 21:59:36 +0000

    Add dependencies for testing

--- one-day-poc-server/prototype/build.gradle.kts
+++ one-day-poc-server/prototype/build.gradle.kts
@@ -19,6 +19,15 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm:2.3.3")
+    testImplementation("io.ktor:ktor-server-test-host:2.3.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("io.mockk:mockk:1.13.4")
     testImplementation(kotlin("test"))
 }
 
---
+++ one-day-poc-server/prototype/src/test/kotlin/integration/PrototypeModuleTest.kt
@@ -0,0 +1,13 @@
+package kcl.seg.rtt.prototype.integration
+
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kcl.seg.rtt.prototype.prototypeModule
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class PrototypeModuleTest {
+    
+}
