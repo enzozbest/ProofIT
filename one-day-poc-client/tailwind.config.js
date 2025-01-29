69496486be4c7e8c421c742c8baf2c254f947018
jacelyne tan <jacelynetan122@gmail.com>
2025-01-30 02:28:51 +0700

    Complete shadcn setup
    - Add components.json for shadcn configuration
    - Update Tailwind color configuration to align with shadcn's system
    - Add font 'inter' as default font
    - Add dependencies for shadcn/tailwind
    - Configured alias `@` for cleaner imports
    - Remove temporary Color function
    
    Signed-off-by: jacelyne tan <jacelynetan122@gmail.com>

--- one-day-poc-client/tailwind.config.js
+++ one-day-poc-client/tailwind.config.js
@@ -1,27 +1,61 @@
+import tailwindcss_animate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        background: "rgb(var(--background) / <alpha-value>)",
-        foreground: "rgb(var(--foreground) / <alpha-value>)",
-        muted: "rgb(var(--muted) / <alpha-value>)",
-        "muted-foreground": "rgb(var(--muted-foreground) / <alpha-value>)",
-        primary: "rgb(var(--primary) / <alpha-value>)",
-        "primary-foreground": "rgb(var(--primary-foreground) / <alpha-value>)",
-        secondary: "rgb(var(--secondary) / <alpha-value>)",
-        "secondary-foreground": "rgb(var(--secondary-foreground) / <alpha-value>)",
-        accent: "rgb(var(--accent) / <alpha-value>)",
-        "accent-foreground": "rgb(var(--accent-foreground) / <alpha-value>)",
-        destructive: "rgb(var(--destructive) / <alpha-value>)",
-        "destructive-foreground": "rgb(var(--destructive-foreground) / <alpha-value>)",
-        ring: "rgb(var(--ring) / <alpha-value>)",
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-      },
+      extend: {
+          borderRadius: {
+            lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+          },
+          colors: {
+            background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                card: {
+              DEFAULT: 'hsl(var(--card))',
+                  foreground: 'hsl(var(--card-foreground))'
+            },
+            popover: {
+              DEFAULT: 'hsl(var(--popover))',
+                  foreground: 'hsl(var(--popover-foreground))'
+            },
+            primary: {
+              DEFAULT: 'hsl(var(--primary))',
+                  foreground: 'hsl(var(--primary-foreground))'
+            },
+            secondary: {
+              DEFAULT: 'hsl(var(--secondary))',
+                  foreground: 'hsl(var(--secondary-foreground))'
+            },
+            muted: {
+              DEFAULT: 'hsl(var(--muted))',
+                  foreground: 'hsl(var(--muted-foreground))'
+            },
+            accent: {
+              DEFAULT: 'hsl(var(--accent))',
+                  foreground: 'hsl(var(--accent-foreground))'
+            },
+            destructive: {
+              DEFAULT: 'hsl(var(--destructive))',
+                  foreground: 'hsl(var(--destructive-foreground))'
+            },
+            border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                chart: {
+              '1': 'hsl(var(--chart-1))',
+                  '2': 'hsl(var(--chart-2))',
+                  '3': 'hsl(var(--chart-3))',
+                  '4': 'hsl(var(--chart-4))',
+                  '5': 'hsl(var(--chart-5))'
+            }
+          },
+        fontFamily: {
+          sans: ["Inter", "sans-serif"],
+        },
     },
   },
-  plugins: [],
+  plugins: [tailwindcss_animate],
 };
