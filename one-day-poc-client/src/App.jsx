d3b971b9ce50a387db84a65101cba7158ff86291
Lucia Garces <lucia.garces_gutierrez@kcl.ac.uk>
2025-01-29 14:05:18 +0000

    Add the LandingPage on the App.jsx

--- one-day-poc-client/src/App.jsx
+++ one-day-poc-client/src/App.jsx
@@ -1,47 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import LandingPage from "./pages/LandingPage";
 
 function ColorPreview() {
     return (
         <div className="grid grid-cols-4 gap-4 p-4">
-            <div className="h-16 w-16 bg-primary"></div>
-            <div className="h-16 w-16 bg-secondary"></div>
-            <div className="h-16 w-16 bg-accent"></div>
-            <div className="h-16 w-16 bg-destructive"></div>
+            <div className="h-16 w-16 bg-primary rounded"></div>
+            <div className="h-16 w-16 bg-secondary rounded"></div>
+            <div className="h-16 w-16 bg-accent rounded"></div>
+            <div className="h-16 w-16 bg-destructive rounded"></div>
         </div>
     );
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const buttons = [
+    "Generate code for a chatbot",
+    "Build a mobile app for my service",
+    "Create a documentation site",
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-        <ColorPreview />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <LandingPage buttons={buttons} />
+      <ColorPreview></ColorPreview>
+    </div>
+  );
 }
 
-export default App
+export default App;
