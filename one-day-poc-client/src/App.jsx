869f73353276df16b4d67e1b71b8b7acafe39736
jacelyne tan <jacelynetan122@gmail.com>
2025-01-30 15:27:14 +0700

    Add generator page with placeholder for chat & viewer - includes sidebar, header

--- one-day-poc-client/src/App.jsx
+++ one-day-poc-client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Chat from './pages/Chat'
 import Home from './pages/Home'
+import Generate1 from "./pages/Generate1";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
         <Routes>
             <Route path="/chat" element={<Chat />} />
+            <Route path="/generate" element={<Generate1 />} />
             <Route path="/" element={<Home />} />
         </Routes>
     </>
