e45055788db4f3132a2e1c63fb89eed5cf03fa0a
jacelynetan <jacelynetan122@gmail.com>
2025-02-09 16:53:40 +0000

    Remove Color Preview function

--- one-day-poc-client/src/App.jsx
+++ one-day-poc-client/src/App.jsx
@@ -6,16 +6,6 @@ import { useState } from 'react';
 import './App.css';
 import LandingPage from "./pages/LandingPage";
 
-function ColorPreview() {
-    return (
-        <div className="grid grid-cols-4 gap-4 p-4">
-            <div className="h-16 w-16 bg-primary rounded"></div>
-            <div className="h-16 w-16 bg-secondary rounded"></div>
-            <div className="h-16 w-16 bg-accent rounded"></div>
-            <div className="h-16 w-16 bg-destructive rounded"></div>
-        </div>
-    );
-}
 
 function App() {
     return (
@@ -25,7 +15,6 @@ function App() {
                 <Route path="/chat" element={<Chat />} />
                 <Route path="/generate" element={<Generate1 />} />
             </Routes>
-            <ColorPreview /> {/* If you want to include the color preview */}
         </div>
     );
 }
