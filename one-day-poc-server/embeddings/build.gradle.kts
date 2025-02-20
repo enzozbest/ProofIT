791eae59d02ba9c35763a222f5302b2cb24a4ea7
Enzo Bestetti <enzobestetti3@gmail.com>
2025-02-20 16:25:15 +0000

    Add build.gradle.kts to embeddings and seeder modules and an entry point for the seeder module.

---
+++ one-day-poc-server/embeddings/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "kcl.seg.rtt"
+version = "0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
