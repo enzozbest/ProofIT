354b52357407e151d52b8412cdfa0399351e2782
Markus Meresma <markus.meresma@gmail.com>
2025-02-23 18:53:20 +0200

    Add seeder for component library

--- one-day-poc-server/seeder/src/main/kotlin/Seeder.kt
+++ one-day-poc-server/seeder/src/main/kotlin/Seeder.kt
@@ -1,2 +1,41 @@
-fun seed() {
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.io.File
+
+@Serializable
+data class ComponentMetadata(
+    val components: List<Component>
+)
+
+@Serializable
+data class Component(
+    val descriptiveName: String,
+    val fileName: String,
+)
+
+class Seeder(private val embeddingService: EmbeddingService) {
+    private fun readComponentMetadata(): ComponentMetadata {
+        val metadataFile = File("src/main/components/metadata/components.json")
+        return Json.decodeFromString(metadataFile.readText())
+    }
+
+    private fun readComponentFile(fileName: String): String {
+        val componentFile = File("src/main/components/$fileName")
+        return componentFile.readText()
+    }
+
+
+    suspend fun seedComponents() {
+        val metadata = readComponentMetadata()
+
+        metadata.components.forEach { component ->
+            // Read component code
+            val code = readComponentFile(component.fileName)
+
+            embeddingService.embedAndStore(
+                name = component.descriptiveName,
+                code = code
+            )
+        }
+    }
 }
