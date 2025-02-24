3bbad5b39fc86a076d47caf170eef99cfda2859b
Enzo Bestetti <enzobestetti3@gmail.com>
2025-02-24 12:14:46 +0000

    Consume Embedding API in EmbeddingService.kt

--- one-day-poc-server/embeddings/src/main/kotlin/EmbeddingService.kt
+++ one-day-poc-server/embeddings/src/main/kotlin/EmbeddingService.kt
@@ -1,16 +1,62 @@
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 object EmbeddingConstants {
-    const val EMBEDDING_SERVICE_URL = "http://localhost:7000/embeddings/embed"
+    const val EMBEDDING_SERVICE_URL = "http://localhost:7000/embeddings/"
+    const val EMBED_URL = "$EMBEDDING_SERVICE_URL/embed"
+    const val EMBED_AND_STORE_URL = "$EMBEDDING_SERVICE_URL/new"
 }
+
+@Serializable
+data class EmbedResponse(
+    val status: String,
+    val embedding: String? = null,
+)
 
 object EmbeddingService {
     private val httpClient by lazy { HttpClient(CIO) }
 
-    suspend fun <T> embed(): List<Float> = emptyList()
+    suspend fun <T> embed(
+        data: T,
+        name: String,
+    ): List<Float> {
+        val responseText =
+            httpClient
+                .post(EmbeddingConstants.EMBED_URL) {
+                    setBody(Json.encodeToString(mapOf("text" to data.toString(), "name" to name)))
+                }.bodyAsText()
 
-    suspend fun embedAndStore(name: String, code: String): Boolean = false
+        val embedResponse = runCatching { Json.decodeFromString<EmbedResponse>(responseText) }.getOrNull()
+        return embedResponse?.let { response ->
+            check(response.status == "success") { "Failed to embed provided data!" }
+            response.embedding?.let { embedding ->
+                embedding.split(",").map { it.toFloat() }
+            } ?: emptyList()
+        } ?: emptyList()
+    }
 
-    suspend fun queryEmbeddings(): List<String> = emptyList()
+    suspend fun <T> embedAndStore(
+        name: String,
+        data: T,
+    ): Boolean {
+        val responseText =
+            httpClient
+                .post(EmbeddingConstants.EMBED_AND_STORE_URL) {
+                    setBody(Json.encodeToString(mapOf("name" to name, "text" to data.toString())))
+                }.bodyAsText()
+        val json = runCatching { Json.decodeFromString<JsonObject>(responseText) }.getOrNull()
+        return json?.let { response -> response["status"]?.let { it.jsonPrimitive.content == "success" } } ?: false
+    }
+
+    suspend fun getEmbedding(name: String): List<Float> = emptyList()
+
+    suspend fun semanticSearch(embedding: List<Float>): List<String> = emptyList()
 }
