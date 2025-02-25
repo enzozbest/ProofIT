7e0001fb14df316683468edd0970032badcd9b64
Enzo Bestetti <enzobestetti3@gmail.com>
2025-02-25 15:29:10 +0000

    Write unit tests for embedding service.

--- one-day-poc-server/embeddings/src/main/python/embedding_service/embedder.py
+++ one-day-poc-server/embeddings/src/main/python/embedding_service/embedder.py
@@ -5,13 +5,13 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def embed(text: str):
     """Converts input text into vector embeddings using a huggingface sentence transformer."""
-    try:
-        return normalize(model.encode(text).tolist())
-    except Exception as e:
+    if not isinstance(text, str):
         return None
+    return normalize(model.encode(text).tolist())
+
 
 def normalize(embedding: list[float]) -> list[float]:
     """Normalize the embedding vectors so that they sum up to 1 (or very close to)."""
     embedding = np.array(embedding)
     norm = np.linalg.norm(embedding)
-    return (embedding / norm).tolist() if norm != 0 else embedding
+    return (embedding / norm).tolist() if norm != 0 else embedding.tolist()
