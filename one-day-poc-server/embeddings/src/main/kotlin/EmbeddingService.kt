261bf1baa3fcf70bafac1dfdd5726e170fca34b7
Enzo Bestetti <enzobestetti3@gmail.com>
2025-02-25 13:12:29 +0000

    Refactor the consumer for the embedding service.

--- one-day-poc-server/embeddings/src/main/kotlin/EmbeddingService.kt
+++ one-day-poc-server/embeddings/src/main/kotlin/EmbeddingService.kt
@@ -2,76 +2,58 @@ import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonPrimitive
-
-object EmbeddingConstants {
-    const val EMBEDDING_SERVICE_URL = "http://localhost:7000/embeddings/"
-    const val EMBED_URL = "$EMBEDDING_SERVICE_URL/embed"
-    const val EMBED_AND_STORE_URL = "$EMBEDDING_SERVICE_URL/new"
-    const val SEMANTIC_SEARCH_URL = "$EMBEDDING_SERVICE_URL/semantic-search"
-}
-
-@Serializable
-data class EmbedResponse(
-    val status: String,
-    val embedding: String? = null,
-)
 
 object EmbeddingService {
     private val httpClient by lazy { HttpClient(CIO) }
 
-    suspend fun <T> embed(
-        data: T,
+    suspend fun embed(
+        data: String,
         name: String,
-    ): List<Float> {
+    ): EmbeddingServiceResponse {
+        val payload = mapOf("text" to data, "name" to name)
         val responseText =
             httpClient
                 .post(EmbeddingConstants.EMBED_URL) {
-                    setBody(Json.encodeToString(mapOf("text" to data.toString(), "name" to name)))
+                    setBody(Json.encodeToString(payload))
                 }.bodyAsText()
-
-        val embedResponse = runCatching { Json.decodeFromString<EmbedResponse>(responseText) }.getOrNull()
-        return embedResponse?.let { response ->
-            check(response.status == "success") { "Failed to embed provided data!" } // Throws exception
-            response.embedding?.let { embedding ->
-                embedding.split(",").map { it.toFloat() }
-            } ?: emptyList()
-        } ?: emptyList()
+        val embedResponse =
+            runCatching { Json.decodeFromString<EmbeddingServiceResponse>(responseText) }.getOrElse {
+                throw IllegalStateException("Failed to parse response!")
+            }
+        return embedResponse
     }
 
-    suspend fun <T> embedAndStore(
+    suspend fun embedAndStore(
         name: String,
-        data: T,
-    ): Boolean {
+        data: String,
+    ): EmbeddingStoreResponse {
+        val payload = mapOf("name" to name, "text" to data)
         val responseText =
             httpClient
                 .post(EmbeddingConstants.EMBED_AND_STORE_URL) {
-                    setBody(Json.encodeToString(mapOf("name" to name, "text" to data.toString())))
+                    setBody(Json.encodeToString(payload))
                 }.bodyAsText()
-        val json = runCatching { Json.decodeFromString<JsonObject>(responseText) }.getOrNull()
-        return json?.let { response -> response["status"]?.let { it.jsonPrimitive.content == "success" } } ?: false
+
+        val storeResponse =
+            runCatching { Json.decodeFromString<EmbeddingStoreResponse>(responseText) }.getOrElse {
+                throw IllegalStateException("Failed to parse response!")
+            }
+        return storeResponse
     }
 
-    suspend fun semanticSearch(embedding: List<Float>): List<String> {
+    suspend fun semanticSearch(embedding: List<Float>): SemanticSearchResponse {
+        val payload = mapOf("embedding" to embedding)
         val responseText =
             httpClient
                 .post(EmbeddingConstants.SEMANTIC_SEARCH_URL) {
-                    setBody(Json.encodeToString(mapOf("embedding" to embedding)))
+                    setBody(Json.encodeToString(payload))
                 }.bodyAsText()
-        val json = runCatching { Json.decodeFromString<JsonObject>(responseText) }.getOrNull()
-
-        return json?.let { response ->
-            check(
-                response["status"]?.let { it.jsonPrimitive.content == "success" }
-                    ?: false,
-            ) { "Failed to perform semantic search!" } // Throws exception
-
-            response["matches"]?.let { array -> array.jsonArray.map { it.jsonPrimitive.content } }
-        } ?: emptyList()
+        val searchResponse =
+            runCatching { Json.decodeFromString<SemanticSearchResponse>(responseText) }.getOrElse {
+                throw IllegalStateException("Failed to parse response!")
+            }
+        return searchResponse
     }
 }
