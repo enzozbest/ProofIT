dcac6377713f2dc4fcbf6aec9fbcbfba9f31f90e
jacelynetan <jacelynetan122@gmail.com>
2025-02-26 17:10:21 +0000

    Reformat sidebar components

--- one-day-poc-client/src/components/ui/overlay.jsx
+++ one-day-poc-client/src/components/ui/overlay.jsx
@@ -1,24 +1,22 @@
-import React from "react";
-import {
-    X
-} from "lucide-react";
+import React from 'react';
+import { X } from 'lucide-react';
 
 export const MutedOverlay = ({ isVisible, onClose, children }) => {
-    if (!isVisible) return null;
+  if (!isVisible) return null;
 
-    return (
-        <div className=" absolute w-full h-full bg-black bg-opacity-5 backdrop-blur-md flex flex-col items-center justify-center z-10" >
-            <div className="p-3 w-96 flex flex-col items-center justify-center">
-                {children}
-                <button 
-                    onClick={onClose} 
-                    className="mt-4 px-4 py-2 text-destructive rounded-md"
-                >
-                    <X />
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className=" absolute w-full h-full bg-black bg-opacity-5 backdrop-blur-md flex flex-col items-center justify-center z-10">
+      <div className="p-3 w-96 flex flex-col items-center justify-center">
+        {children}
+        <button
+          onClick={onClose}
+          className="mt-4 px-4 py-2 text-destructive rounded-md"
+        >
+          <X />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MutedOverlay;
