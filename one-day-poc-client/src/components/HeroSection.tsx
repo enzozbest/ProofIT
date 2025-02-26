b3f1799a8d7f7d4f3b6d755ddd85ae8ec079f482
jacelynetan <jacelynetan122@gmail.com>
2025-02-26 17:13:05 +0000

    Amend CTA
    - Changed "Supporting" to "Enabling" following client's suggestion

--- one-day-poc-client/src/components/HeroSection.tsx
+++ one-day-poc-client/src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ const HeroSection: FC = () => {
   return (
     <div className="flex flex-col items-center justify-center text-center mb-5 px-6">
       <h1 className="text-white text-5xl md:text-6xl font-light">
-        Supporting you from
+        Enabling you from
       </h1>
       <h2 className="text-white text-6xl md:text-7xl font-bold text-[#f7a6ff]">
         day one
