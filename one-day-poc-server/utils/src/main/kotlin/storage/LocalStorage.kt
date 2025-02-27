f1dfca4b9b9e992bf70d12d35bbfef44307625dd
Enzo Bestetti <enzobestetti3@gmail.com>
2025-02-27 11:54:09 +0000

    Add File storage functionality.
    In this commit I introduce a wrapper for file storage to be used throughout the application.
    It provides an abstraction from whichever storage service we use, including whether we are storing files locally or remotely.
    From now on, you can call the functions from StorageService to store, get, and delete files.

---
+++ one-day-poc-server/utils/src/main/kotlin/storage/LocalStorage.kt
@@ -0,0 +1,43 @@
+package storage
+
+import java.io.File
+import kotlin.io.path.Path
+
+object LocalStorage {
+    /**
+     * Function to store a file in local storage.
+     * If the call succeeds, the full system path to the file is returned. Otherwise, an empty string is returned.
+     *
+     * @param path The path to store the file.
+     * @param name The name of the file.
+     * @param file The file to store.
+     * @return The full system path to the file if the call succeeds, an empty string otherwise.
+     */
+    fun storeFile(
+        path: String,
+        name: String,
+        file: File,
+    ): String =
+        runCatching {
+            file.copyTo(Path(path, name).toFile(), overwrite = true)
+            file.delete()
+        }.getOrNull()?.let { Path(path, name).toString() } ?: ""
+
+    /**
+     * Function to retrieve a file from local storage.
+     * If the file is found, it is returned. Otherwise, null is returned.
+     * @param path The full path to the file.
+     * @return The contents of the file as a ByteArray. If the file is not found, null is returned.
+     */
+    fun getFile(path: String): ByteArray? =
+        runCatching {
+            Path(path).toFile().toString().toByteArray()
+        }.getOrNull()
+
+    /**
+     * Function to delete a file from local storage.
+     *@param path The full path to the file.
+     * @return true if the file was deleted, false otherwise.
+     */
+    fun deleteFile(path: String) = runCatching { Path(path).toFile().delete() }.isSuccess
+}
