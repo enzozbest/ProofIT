05ba02c80384cdb79e27968a2550d1b236f30aaa
IsabellaMcleanKCL <isabella.mclean@kcl.ac.uk>
2025-03-05 04:23:36 +0000

    Update ChatPage tests

--- one-day-poc-client/src/__tests__/ChatPage.test.jsx
+++ one-day-poc-client/src/__tests__/ChatPage.test.jsx
@@ -1,7 +1,7 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen, waitFor,fireEvent } from '@testing-library/react'
 import ChatScreen from '../pages/ChatScreen';
 import '@testing-library/jest-dom';
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, useLocation } from "react-router-dom";
 import { vi, test, expect, beforeEach } from "vitest";
 import userEvent from '@testing-library/user-event';
 
@@ -9,7 +9,10 @@ globalThis.fetch = vi.fn();
 const mockSetPrototype = vi.fn();
 const mockSetPrototypeId = vi.fn();
 const consoleErrorSpy = vi.spyOn(console, 'error').mockImplementation(() => {});
-
+vi.mock('react-router-dom', () => ({
+    ...vi.importActual('react-router-dom'),
+    useLocation: vi.fn(),
+}));
 
 
 // Ensure mocks are reset before each test
@@ -107,5 +110,30 @@ test("Invalid post request", async () =>{
     await waitFor(() => expect(consoleErrorSpy).toHaveBeenCalledTimes(2));
 })
 
+test("Clicking send button sends a message", async () =>{
+    fetch.mockResolvedValueOnce({
+        ok: true,
+        text: vi.fn().mockResolvedValue("Mock LLM response"),
+    });
+    render(
+        <MemoryRouter>
+            <ChatScreen setPrototype={mockSetPrototype} setPrototypeId={mockSetPrototypeId}/>
+        </MemoryRouter>
+    );
+
+    const userchat = screen.getByPlaceholderText(/How can we help you today?/i);
+    await userEvent.type(userchat, 'Hello!')
+    const sendButton = screen.getByText("Send");
+    fireEvent.click(sendButton);
+    await waitFor(() => {
+        expect(userchat).toHaveValue('')
+    },{timeout: 3000})
 
 
+    expect(fetch).toHaveBeenCalledWith("http://localhost:8000/api/chat/json", {
+        method: "POST",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+        body:expect.any(String),
+    });
+})
