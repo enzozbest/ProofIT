e4de91f29bd7d9d76ba43d2b8274f1f229ecbc9a
rSamaei <k23005640@kcl.ac.uk>
2025-03-07 11:26:53 +0000

    Create test for global.d.ts

--- one-day-poc-client/vitest.config.js
+++ one-day-poc-client/vitest.config.js
@@ -7,28 +7,31 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-    coverage: {
-      reporter: ['text', 'html', 'lcov'],
-    exclude:[
-        'postcss.config.js',
-        'tailwind.config.js',
-        '.eslintrc.js',
-        'eslint.config.js',
-        'vite.config.js',
-        'vitest.config.js',
-        'src/__tests__',
-        'src/components/ui'
-        ]
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.js',
+        moduleNameMapper: {
+            '\\.(svg)$': '<rootDir>/src/__mocks__/fileMock.ts'
+        },
+        coverage: {
+            reporter: ['text', 'html', 'lcov'],
+            exclude:[
+                'postcss.config.js',
+                'tailwind.config.js',
+                '.eslintrc.js',
+                'eslint.config.js',
+                'vite.config.js',
+                'vitest.config.js',
+                'src/__tests__',
+                'src/components/ui'
+            ]
+        },
     },
-  },
 });
