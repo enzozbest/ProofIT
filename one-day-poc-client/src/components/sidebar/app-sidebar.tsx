2f2df6cb97bfe903942c8385676cb14a401da4fe
jacelynetan <jacelynetan122@gmail.com>
2025-03-10 16:50:08 +0000

    Restructure component files into folders based on overarching component

--- one-day-poc-client/src/components/sidebar/app-sidebar.tsx
+++ one-day-poc-client/src/components/sidebar/app-sidebar.tsx
@@ -14,9 +14,9 @@ import {
   History,
 } from "lucide-react"
 
-import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavUser } from "@/components/nav-user"
+import { NavMain } from "@/components/sidebar/nav-main"
+import { NavProjects } from "@/components/sidebar/nav-projects"
+import { NavUser } from "@/components/sidebar/nav-user"
 // import { TeamSwitcher } from "@/components/team-switcher"
 import {
   Sidebar,
