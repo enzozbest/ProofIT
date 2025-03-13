42cbb3ec30729a188e6bddd12fda96d8671a0325
jacelynetan <jacelynetan122@gmail.com>
2025-03-13 01:19:12 +0000

    Refactor messages-box components into `messages-bubbles.tsx`
    - created `message-bubbles.tsx` component to handle bubble, content and timestamp for better maintainability and clearer structure

--- one-day-poc-client/src/components/chat/messages-box.jsx
+++ one-day-poc-client/src/components/chat/messages-box.jsx
@@ -3,6 +3,7 @@ import { TypographyMuted, TypographyInlineCode,} from "@/components/ui/typograph
 import ChatMessage from "@/hooks/Chat";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { MessageBubble, MessageBubbleContent, MessageBubbleTimestamp } from "./message-bubble";
 
 
 export function MessageBox({ sentMessages }) {
@@ -20,74 +21,39 @@ export function MessageBox({ sentMessages }) {
     return(
         <>
             {/* Message List */}
-            <div className="flex flex-col flex-1 overflow-y-auto p-2.5 text-secondary-foreground">
+            <div className="flex flex-col flex-1 overflow-y-auto gap-1 p-1 text-secondary-foreground">
                 {/*List of messages*/}
                 {sentMessages.map((msg, index) => (
-                    msg.role === "User" ? (
-                        <div
-                            key={index}
-                            className="p-2.5 bg-muted/90 rounded mb-2.5 self-end break-words max-w-[70%]"
-                        >
-                            
-                            {msg.content && (
-                                <Markdown
-                                    key={index}
-                                    remarkPlugins={[remarkGfm]} 
-                                    components={{
-                                        code({ node, inline, className, children, ...props }) {
-                                            return inline ? (
+                    <MessageBubble
+                    key={index}
+                    variant={msg.role == "User" ? "user" : "llm"}
+                    >
+                        <MessageBubbleContent>
+                        {msg.content && (
+                            <Markdown
+                                key={index}
+                                remarkPlugins={[remarkGfm]} 
+                                components={{
+                                    code({ node, inline, className, children, ...props }) {
+                                        return inline ? (
+                                            <TypographyInlineCode {...props}>{children}</TypographyInlineCode>
+                                        ) : (
+                                            <pre className="whitespace-pre-wrap pt-2">
                                                 <TypographyInlineCode {...props}>{children}</TypographyInlineCode>
-                                            ) : (
-                                                <pre className="whitespace-pre-wrap pt-2">
-                                                    <TypographyInlineCode {...props}>{children}</TypographyInlineCode>
-                                                </pre>
-                                            );
-                                        }
-                                    }}
-                                >
-                                    {msg.content}
-                                </Markdown>
-                            )}
+                                            </pre>
+                                        );
+                                    }
+                                }}
+                            >
+                                {msg.content}
+                            </Markdown>
+                        )}
 
-                            <TypographyMuted> {new Date(msg.timestamp).toLocaleString("en-GB",{
-                                hour: "2-digit",
-                                minute:"2-digit",
-                                hour12: true
-                            })} </TypographyMuted>
-                        </div>
-                    ) : (
-                        <div
-                            key={index}
-                            className="p-2.5 bg-transparent rounded mb-2.5 self-start break-words w-full"
-                        >
-                            {msg.content && (
-                                <Markdown
-                                    key={index}
-                                    remarkPlugins={[remarkGfm]} 
-                                    components={{
-                                        code({ node, inline, className, children, ...props }) {
-                                            return inline ? (
-                                                <TypographyInlineCode {...props}>{children}</TypographyInlineCode>
-                                            ) : (
-                                                <pre className="whitespace-pre-wrap pt-2">
-                                                    <TypographyInlineCode {...props}>{children}</TypographyInlineCode>
-                                                </pre>
-                                            );
-                                        }
-                                    }}
-                                >
-                                    {msg.content}
-                                </Markdown>
-                            )}
-
-                            <TypographyMuted> {new Date(msg.timestamp).toLocaleString("en-GB",{
-                                hour: "2-digit",
-                                minute:"2-digit",
-                                hour12: true
-                            })} </TypographyMuted>
-                        </div>
+                            <MessageBubbleTimestamp timestamp={msg.timestamp} />
+                        </MessageBubbleContent>
+                   </MessageBubble>
                     )
-                ))}
+                )}
                 {/* Dummy div to enable auto-scrolling */}
                 <div ref={recentMessageRef} />
             </div>
