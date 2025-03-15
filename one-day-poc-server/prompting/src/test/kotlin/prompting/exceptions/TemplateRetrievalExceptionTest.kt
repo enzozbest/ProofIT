e1032724b8b2da00735d8e0cc52e94c701d2e341
Krystian Augustynowicz <krysaugust49@gmail.com>
2025-03-15 05:58:52 +0000

    Test `TemplateRetrievalException` created correctly

--- one-day-poc-server/prompting/src/test/kotlin/prompting/exceptions/TemplateRetrievalExceptionTest.kt
+++ one-day-poc-server/prompting/src/test/kotlin/prompting/exceptions/TemplateRetrievalExceptionTest.kt
@@ -1,5 +1,12 @@
 package prompting.exceptions
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 
-class TemplateRetrievalExceptionTest {}
+class TemplateRetrievalExceptionTest {
+    @Test
+    fun testTemplateRetrievalException() {
+        val exception = TemplateRetrievalException("Test error message")
+        assertEquals(exception.message, "Test error message")
+    }
+}
