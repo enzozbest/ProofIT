2bab334772431921dc24e50d41a763c588a191e0
rSamaei <k23005640@kcl.ac.uk>
2025-03-18 14:35:35 +0000

    Add userId to ConversationEntity and update message sender logic

--- one-day-poc-server/database/src/main/kotlin/database/tables/chats/ChatTables.kt
+++ one-day-poc-server/database/src/main/kotlin/database/tables/chats/ChatTables.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.sql.ReferenceOption
 
 object ChatMessageTable : UUIDTable("chat_messages") {
     val conversationId = reference("conversation_id", ConversationTable, onDelete = ReferenceOption.CASCADE)
-    val senderId = varchar("sender_id", 128)
+    val isFromLLM = bool("is_from_llm")
     val content = text("content")
     val timestamp = timestamp("timestamp")
 }
@@ -14,4 +14,5 @@ object ChatMessageTable : UUIDTable("chat_messages") {
 object ConversationTable : UUIDTable("conversations") {
     val name = varchar("name", 255)
     val lastModified = timestamp("last_modified")
+    val userId = varchar("user_id", 128).index()
 }
