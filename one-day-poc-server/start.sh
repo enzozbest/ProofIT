8f676c5631b8818f3791b7f750712448227e02c2
Enzo Bestetti <enzobestetti3@gmail.com>
2025-03-18 10:25:34 +0000

    Introduce scripts to run the backend.
    NOTE: you may need to mark start.sh (Linux and macOS) as executable with
    
    `chmod +x start.sh`

---
+++ one-day-poc-server/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if the virtual environment directory exists
+if [ ! -d "./embeddings/src/main/python/venv" ]; then
+    echo "Virtual environment for Python microservice not found. Creating venv and installing requirements..."
+    python3.10 -m venv ./embeddings/src/main/python/venv
+    source ./embeddings/src/main/python/venv/bin/activate
+    pip install -r ./embeddings/src/main/python/requirements.txt
+else
+    echo "Virtual environment for Python microservicec found! Activating..."
+    source ./embeddings/src/main/python/venv/bin/activate
+fi
+
+# Run the Python module in the background and then run Gradle
+python3.10 ./embeddings/src/main/python/information_retrieval/__main__.py & ./gradlew run
