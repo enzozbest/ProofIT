659c6fc110d75c1a5d88816fdbd80fd6de7cc7fa
jacelynetan <jacelynetan122@gmail.com>
2025-03-20 00:16:29 +0000

    Amend imports and add to tests

--- one-day-poc-client/src/__tests__/components/chat/MessageBox.test.jsx
+++ one-day-poc-client/src/__tests__/components/chat/MessageBox.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { describe, test, expect } from 'vitest';
-import { MessageBox } from '../../../components/chat/messages-box.jsx';
+import { MessageBox } from '../../../components/chat/MessagesBox.jsx';
 import '../../mocks/message-box.mock.jsx';
 
 describe('MessageBox Component', () => {
