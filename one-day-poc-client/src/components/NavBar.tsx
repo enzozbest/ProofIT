a02f44cf7479d8987c0bc18485e441ec8eaf4ddd
Lucia Garces <lucia.garces_gutierrez@kcl.ac.uk>
2025-03-20 16:10:38 +0000

    Change from absolute to fixed

--- one-day-poc-client/src/components/NavBar.tsx
+++ one-day-poc-client/src/components/NavBar.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BotMessageSquare } from 'lucide-react';
 import { useAuth } from '../contexts/AuthContext';
+import Logo from '@/components/Logo';
 
 /**
  * NavBar component provides the application's top navigation bar with authentication controls.
- * 
+ *
  * Displays the application logo/name and provides authentication functionality with
  * sign-in and sign-out buttons that adapt based on the user's authentication status.
  * The component makes API calls to handle authentication actions.
- * 
+ *
  * @component
  * @returns {JSX.Element} A navigation bar with application branding and authentication buttons
  */
@@ -16,26 +16,20 @@ const NavBar: React.FC = () => {
   const { isAuthenticated, login, logout } = useAuth();
 
   return (
-    <nav className="absolute top-0 left-0 w-full flex justify-between items-center px-10 py-6 z-10">
-      <div className="flex items-center gap-2 text-xl ">
-        <BotMessageSquare className="w-6 h-6 " />
-        <span className="font-normal">
-          PROOF -<span className="font-bold"> IT!</span>
-        </span>
-      </div>
-
+    <nav className="fixed top-0 left-0 w-full h-16 flex justify-between items-center px-10 py-6 z-50">
+      <Logo />
       <div className="flex gap-2">
         {isAuthenticated ? (
           <button
             onClick={logout}
-            className="border-2 border-white bg-transparent  px-6 py-2 rounded-full hover:bg-white hover:text-[#731ecb] transition"
+            className="border-2 border-white bg-transparent px-6 py-2 rounded-full hover:bg-white hover:text-[#731ecb] transition"
           >
             Log Out
           </button>
         ) : (
           <button
             onClick={login}
-            className="border-2 border-white  bg-transparent px-6 py-2 rounded-full hover:bg-white hover:text-[#731ecb] transition"
+            className="border-2 border-white bg-transparent px-6 py-2 rounded-full hover:bg-white hover:text-[#731ecb] transition"
           >
             Sign In
           </button>
