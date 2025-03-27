cbe959534829878653c9aadebe08f4a30b54561c
Enzo Bestetti <enzobestetti3@gmail.com>
2025-03-27 10:10:09 +0000

    Reformat test files in utils and add extra tests in chat.

--- one-day-poc-server/utils/src/main/kotlin/utils/aws/STSInteractor.kt
+++ one-day-poc-server/utils/src/main/kotlin/utils/aws/STSInteractor.kt
@@ -4,6 +4,9 @@ import aws.sdk.kotlin.services.sts.StsClient
 import aws.sdk.kotlin.services.sts.model.AssumeRoleRequest
 import aws.smithy.kotlin.runtime.auth.awscredentials.Credentials
 
+/**
+ * Object to allow interaction with the AWS Security Token Service (STS) to assume a role.
+ */
 object STSInteractor {
     suspend fun assumeRole(
         stsClient: StsClient,
