e880b3e65f388d4a1c5dd1489046a2daff0a182c
jacelynetan <jacelynetan122@gmail.com>
2025-03-27 00:53:05 +0000

    Fix appearance of new chat button in sidebar

--- one-day-poc-client/src/components/sidebar/AppSidebar.tsx
+++ one-day-poc-client/src/components/sidebar/AppSidebar.tsx
@@ -11,6 +11,7 @@ import {
   SidebarFooter,
   SidebarHeader,
   SidebarRail,
+  SidebarMenuButton
 } from '@/components/ui/Sidebar';
 import { useConversation } from '@/contexts/ConversationContext';
 import { Button } from '@/components/ui/Button';
@@ -44,15 +45,16 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     <Sidebar collapsible="icon" className="bg-background/85" {...props}>
       <SidebarHeader></SidebarHeader>
       <SidebarContent>
-        <div className="px-4 py-2">
-          <Button
-            onClick={createConversation}
-            variant="outline"
-            className="w-full flex items-center"
-          >
-            <PlusCircle size={16} className="mr-2" />
-            New Chat
-          </Button>
+        <div className="relative flex w-full min-w-0 flex-col p-2">
+          <SidebarMenuButton 
+              data-testid="new-chat-button"
+              onClick={createConversation}
+              tooltip={"New Chat"} 
+              className={"bg-transparent w-full justify-center items-center flex min-w-0 gap-1 border border-input group-data-[collapsible=icon]:border-0"}>
+                  {PlusCircle && <PlusCircle className=""/>}
+                  <span>{"New Chat"}</span>
+              
+          </SidebarMenuButton>
         </div>
         <NavMain items={navMainData} />
       </SidebarContent>
